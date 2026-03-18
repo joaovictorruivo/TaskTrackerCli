37d2b012c33dd05aa28927e7bf4d64583bce58d4
João Victor <joao4920oliveira@gmail.com>
2026-03-17 23:28:25 -0300

    feat: adicionado sistema de criação e listagem de tarefas

--- TaskTrackerCli/Program.cs
+++ TaskTrackerCli/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TaskTrackerCli;
+
+List<Tarefa> tarefas = new List<Tarefa>();
+int proximoId = 1;
+
+while (true)
+{
+    Console.WriteLine("\n--- GERENCIADOR DE TAREFAS ---");
+    Console.WriteLine("1. Adicionar Tarefa");
+    Console.WriteLine("2. Listar Tarefas");
+    Console.WriteLine("3. Sair");
+    Console.Write("Escolha uma opção: ");
+
+    var opcao = Console.ReadLine();
+
+    if (opcao == "1")
+    {
+        Console.Write("Digite a descrição: ");
+        var desc = Console.ReadLine() ?? "";
+        tarefas.Add(new Tarefa { Id = proximoId++, Descricao = desc });
+        Console.WriteLine("Tarefa adicionada!");
+    }
+    else if (opcao == "2")
+    {
+        Console.WriteLine("\nSua Lista:");
+        foreach (var t in tarefas)
+        {
+            Console.WriteLine($"[{t.Id}] {t.Descricao} - {(t.Concluida ? "Check" : "Pendente")}");
+        }
+    }
+    else if (opcao == "3") break;
+}
