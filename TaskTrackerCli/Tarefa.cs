5a581f4c9d0ac267b64c9d8e0f3721ce394adeb7
João Victor <joao4920oliveira@gmail.com>
2026-03-17 23:27:24 -0300

    feat: estrutura inicial do projeto e modelo de tarefa

---
+++ TaskTrackerCli/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskTrackerCli
+{
+    internal class Tarefa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public bool Concluida { get; set; } = false;
+
+    }
+}
